d65f5bb244810fb9084b02991e6d28c6432fddad
Lautaro261 <nahuelygoofy@gmail.com>
2023-02-24 17:13:54 -0300

    Ejercicio 1 listo

--- 06-React-Intro/homework/01 - Exercises/src/App.js
+++ 06-React-Intro/homework/01 - Exercises/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Bienvenido from './components/Bienvenido.jsx';
+import { studentName } from './components/Bienvenido.jsx';
 
 export default function App() {
    return (
       <div>
-         <Bienvenido />
+         <Bienvenido/>
       </div>
    );
 }
--- 06-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
+++ 06-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 
-const studentName = "";
+const studentName = "Lautaro";
 const techSkills = ["Html", "Css", "JavaScript", "React", "Redux"];
 const alerts = { m1: "Aprobado", m2: "En curso" };
 
-export default function Bienvenido() {
+export default function Bienvenido(props) {
   // el código de tu componente acá
-  return;
+  return(
+    <div>
+      <h1>Holiss</h1>
+      <h3>{studentName}</h3>
+      <ul>
+        {
+          techSkills.map((item)=>{
+            return <li>{item}</li>
+          })
+        }
+      </ul>
+    </div>
+  )
 }
 
 // Esto lo exportamos para los tests
