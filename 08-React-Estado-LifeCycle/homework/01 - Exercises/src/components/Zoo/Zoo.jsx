c9c04bc41ef6859339f5262b2013f76972a78207
Lautaro261 <nahuelygoofy@gmail.com>
2023-03-10 01:47:50 -0300

    no funciona :c

--- 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Zoo/Zoo.jsx
+++ 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Zoo/Zoo.jsx
@@ -7,9 +7,22 @@ import './Zoo.module.css';
 
 export default function Zoo() {
    /* Escribe acá tu código */
+   const [zoo, setZoo] = React.useState({
+      zooName:'',
+      animals:[],
+      species:[],
+      allAnimals:[],
+   })
+
+    const handleInputChange = (event)=>{
+      console.log(event)
+       setZoo(zoo.zooName = event) 
+   }  
    return (
       <div>
-         <h1>-</h1>
+         <label>zoo Name</label>
+         <input value={zoo.zooName}  onChange={()=>handleInputChange} ></input>
+         <h1>{zoo.zooName}</h1>
       </div>
    );
 }
