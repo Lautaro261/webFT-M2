6a265e177641ec2052e508fac4e23d4061d55902
Lautaro261 <nahuelygoofy@gmail.com>
2023-03-11 12:10:44 -0300

    Parte 1 Listo

--- 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Species/Species.jsx
+++ 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Species/Species.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 
-export default function Species () {
+export default function Species (props) {
   return (
-    <div>
-
-    </div>
+    <>
+      <h2>Species</h2>
+      <div>
+        {props.species.map((specie, index)=>{
+          return(
+            <button 
+            key={index} 
+            onClick={props.handleSpecies} 
+            value={specie} >{specie}</button>
+          )
+        })}
+        <button onClick={props.handleAllSpecies}>All Animals</button>
+      </div>
+    </>
   )
 }
--- 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Zoo/Zoo.jsx
+++ 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Zoo/Zoo.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 // eslint-disable-next-line no-unused-vars
 import Animals from '../Animals/Animals';
 // eslint-disable-next-line no-unused-vars
@@ -15,14 +15,66 @@ export default function Zoo() {
    })
 
     const handleInputChange = (event)=>{
-      console.log(event)
-       
+      console.log(event.target.value)
+      setZoo({
+         ...zoo,
+         // PARA MODIFICAR EL OBJETO INICIAL, SE LO COPIA CON ...zoo 
+         // LUEGO SE PISA LA PROPIEDAD QUE QUIERO MODIFICAR 
+         zooName: event.target.value ,
+         // PARA ACCEDER AL VALOR DE INPUT, SIEMPRE USAR event.target.value
+      })
    }  
+
+   React.useEffect(()=>{
+      fetch('http://localhost:3001/zoo')
+      .then((res) => res.json())
+      .then((data) =>
+      setZoo({
+         ...zoo,
+         animals: data.animals,
+         species: data.species,
+         allAnimals: data.animals,
+      })
+   )
+   .catch((error) => console.log(error));},[])
+
+   const handleSpecies=(event)=>{
+      // GUARDO EN specie EL valor del boton que recibo como props
+      const specie = event.target.value
+      // COPIO el objeto zoo pisando la propiedad animals CON un filtrado de TODOS los animales
+      //filter recibe un argumento y una condicion a evaluar, si es VERDAD retorna, FALSO no retorna 
+      setZoo({
+         ...zoo,
+         animals: zoo.allAnimals.filter(animal=> animal.specie === specie),
+      })
+   }
+   // recibe el value del boton, cual especie hice click, 
+   // modifico el estado del componente zoo.jsx
+   //filtro los animales que complen con la condicion de tener la misma especie que el value 
+   // retorno un array con los animales que dieron VERDAD 
+
+   const handleAllSpecies=()=>{
+      setZoo({
+         ...zoo,
+         animals: zoo.allAnimals,
+      })
+   }
+
+
+
    return (
       <div>
-         <label>zoo Name</label>
-         <input value={zoo.zooName}  onChange={()=>handleInputChange} ></input>
+         <label>Zoo Name:</label>
+         <input value={zoo.zooName}  onChange={handleInputChange} ></input>
          <h1>{zoo.zooName}</h1>
+         <Species
+         species={zoo.species} 
+         handleSpecies={handleSpecies} 
+         handleAllSpecies={handleAllSpecies}
+         />
+         <Animals 
+         animals={zoo.animals}
+         />
       </div>
    );
 }
