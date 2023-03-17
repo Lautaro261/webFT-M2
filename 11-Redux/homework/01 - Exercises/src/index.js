c0d720dbdc44dfa83cb7a8c6d39d8bb218384621
Lautaro261 <nahuelygoofy@gmail.com>
2023-03-17 17:35:36 -0300

    Primera parte Listo

--- 11-Redux/homework/01 - Exercises/src/index.js
+++ 11-Redux/homework/01 - Exercises/src/index.js
@@ -3,10 +3,10 @@ const contador = require("./reducer");
 const { incremento, decremento } = require("./actions");
 
 // En esta línea creamos nuestro store. Pasándole como parámetro nuestro Reducer
-var store;
+var store = createStore(contador);
 
 // Obtenemos el elemento con el id `valor`.
-var valor;
+var valor= document.querySelector('#valor');
 
 // Esta función nos va a servir para actualizar nuestro DOM con el valor que tengamos en nuestro Store.
 // En el primer render y cada vez que nos subscribamos al Store.
@@ -14,11 +14,39 @@ var valor;
 function renderContador() {
   // Obtenemos la propiedad 'contador' de nuestro store:
   // Seteamos el número obtenido como texto dentro del elemento con id 'valor':
+  const contador = store.getState().contador;
+  valor.innerHTML=contador;
 }
 
 // Ejecutamos la función 'renderContador':
+renderContador()
+
+
 
 // Nos subscribimos al store pasándole la misma función. Así cada vez que llegue una acción, ejecutamos la función:
-
+store.subscribe(renderContador)
 // Por último, utilizamos los botones de nuestro HTML para que cada vez que hagamos click,
 // hagan un dispatch al store de la acción correspondiente:
+
+const incrementoButton = document.querySelector('#incremento');
+const decremetoButton = document.querySelector('#decremento');
+const incrementoImpar = document.querySelector('#incrementoImpar');
+const incrementoAsync = document.querySelector('#incrementoAsync');
+
+incrementoButton.addEventListener("click", ()=>{
+  store.dispatch(incremento())
+});
+
+decremetoButton.addEventListener('click', ()=>{
+  store.dispatch(decremento());
+})
+
+incrementoImpar.addEventListener('click', ()=>{
+ if(valor.innerHTML% 2 !== 0) store.dispatch(incremento())
+})
+
+incrementoAsync.addEventListener('click', ()=>{
+  setTimeout(()=>{
+    store.dispatch(incremento())
+  }, 2000)
+})
