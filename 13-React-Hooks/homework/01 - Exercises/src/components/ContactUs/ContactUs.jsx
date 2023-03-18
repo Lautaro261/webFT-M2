ba1eef9abad8681b773364d73dd965d5c6e1df23
Lautaro261 <nahuelygoofy@gmail.com>
2023-03-18 01:20:48 -0300

    Parte 1 lista

--- 13-React-Hooks/homework/01 - Exercises/src/components/ContactUs/ContactUs.jsx
+++ 13-React-Hooks/homework/01 - Exercises/src/components/ContactUs/ContactUs.jsx
@@ -1,18 +1,60 @@
-import React from "react";
+import React  from "react";
+import { useDispatch} from 'react-redux';
+import { enviarForm } from "../../redux/actions/actions";
 
 const ContactUs = () => {
+
+  // crear un estado local 
+  const [form, setForm]= React.useState({
+    nombre:'',
+    email:'',
+    asunto:'',
+    mensaje:'',
+  })
+
+
+  // funcion que escucha el evento del cambio event.target.value y name y por ultimo seteo el estado local 
+  // en la propiedad variable con el valor que tomo del input 
+const handleInput =(event)=>{
+  const value = event.target.value;
+  const property = event.target.name;
+  setForm({
+    ...form,
+    [property]:value,
+  })
+}
+
+// instancio Dispatch con metodos que me permite despechar actions 
+const dispatch = useDispatch()
+
+
+const handleSubmit=(evento)=>{
+  evento.preventDefault()
+  dispatch(enviarForm(form));
+  setForm({
+    nombre:'',
+    email:'',
+    asunto:'',
+    mensaje:'',
+  })
+}
+
+
+
+
+
   return (
     <div>
-      <form className="contactBg">
+      <form className="contactBg" >
         <label htmlFor="nombre">Nombre: </label>
-        <input name="nombre" />
+        <input name="nombre"  value={form.nombre} onChange={handleInput}/>
         <label htmlFor="email">Email: </label>
-        <input name="email" />
+        <input name="email" value={form.email} onChange={handleInput}/>
         <label htmlFor="asunto">Asunto: </label>
-        <input name="asunto" />
+        <input name="asunto" value={form.asunto} onChange={handleInput}/>
         <label htmlFor="mensaje">Mensaje: </label>
-        <input name="mensaje" />
-        <button>Enviar</button>
+        <input name="mensaje" value={form.mensaje} onChange={handleInput}/>
+        <button onClick={handleSubmit}>Enviar</button>
       </form>
     </div>
   );
--- 13-React-Hooks/homework/01 - Exercises/src/components/InfoEnviada/InfoEnviada.jsx
+++ 13-React-Hooks/homework/01 - Exercises/src/components/InfoEnviada/InfoEnviada.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import {useSelector} from 'react-redux';
 
+//ESTADO LOCAL
 const InfoEnviada = () => {
+   const [informacion, setInformacion] = React.useState({
+      nombre:'',
+      email:'',
+      asunto:'',
+      mensaje:'',
+   }) 
+//ME TRAIGO INFORMACION DEL ESTADO GLOBAL
+   const {formulario} = useSelector((state)=>{
+      return state
+   })
+
+// manejo la info cuando se monta, cuando cambia y cuando se desmonta el componente
+   React.useEffect(()=>{
+      setInformacion(formulario)
+   },[formulario])
+
+
    return (
       <div>
          {/*Aquí puedes renderizar la información*/}
+         <h1>ESTA ES LA INFORMACIÓN QUE ENVIASTE...</h1>
+         <h3>{informacion.nombre}</h3>
+         <h3>{informacion.email}</h3>
+         <h3>{informacion.asunto}</h3>
+         <h3>{informacion.mensaje}</h3>
       </div>
    );
 };
